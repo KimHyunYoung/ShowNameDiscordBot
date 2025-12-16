82719268a139f2100a5f44f211f190e9a39cdfa8
infinty <hangle8@naver.com>
2025-12-16 21:04:46 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import os
-import emoji
+import re
 
 
 intents = discord.Intents.default()
@@ -9,11 +9,9 @@ intents.members = True  # 멤버 목록 접근 허용
 intents.message_content = True   # 메시지 내용 읽기 허용
 intents.voice_states = True   # 음성 상태 접근 허용
 
-
-
-def remove_emojis(text):
-    return emoji.replace_emoji(text, replace='')
-
+def remove_non_alnum(text):
+    # 영어 대소문자(A-Z, a-z)와 숫자(0-9)만 남기고 나머지는 모두 제거
+    return re.sub(r'[^A-Za-z0-9]', '', text)
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
@@ -34,7 +32,7 @@ async def 참여자(ctx):
             member_names = []
             for members in vc.members:
                 nickname = members.display_name  # 닉네임 가져오기
-                nickname = remove_emojis(nickname)  # 이모지 제거
+                nickname = remove_non_alnum(nickname)  # 이모지 제거
                 parts = nickname.split("/")
                 if len(parts) > 1:
                     first = parts[0]
